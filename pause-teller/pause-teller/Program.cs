13388767fe74db11345c5a437c9e73319672e835
remcp <114666486+remcp@users.noreply.github.com>
2022-10-18 19:39:52 +0200

    Add files via upload

---
+++ pause-teller/pause-teller/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using pause_teller;
+
+namespace Pause_teller
+{
+    unsafe class Exe
+    {
+        public static void Main()
+        {
+            Teller teller = new Teller();
+            Aanuit aanuit = new Aanuit();
+            bool run = true;
+            Boolean tel = true;
+            long time = 0;
+            long* ptrtime = &time;
+            int timer = 0;
+            while (run == true)
+            {
+                tel = aanuit.Switch(tel);
+                if (tel == true)
+                {
+                    long milliseconds = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+                    timer = teller.Tel(timer, ptrtime, milliseconds);
+                }
+            }
+        }
+    }
+}
---
+++ pause-teller/pause-teller/Teller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pause_teller
+{
+    unsafe class Teller
+    {
+        public int Tel(int timer, long* ptrtime, long milliseconds)
+        {
+
+            if (milliseconds - *ptrtime >= 1000)
+            {
+                timer++;
+                Console.WriteLine(timer);
+                *ptrtime = milliseconds;
+            }
+
+            return timer;
+        }
+    }
+}
