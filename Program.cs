a31af9c28d0ec32a120bb102578c663c3ad89099
remcp <114666486+remcp@users.noreply.github.com>
2022-10-18 18:20:12 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teller_pause
+{
+    class Exe
+    {
+        static void Main()
+        {
+            Teller teller = new Teller();
+            bool run = true;
+            Boolean tel = true;
+            long time = 0;
+            int timer = 0;
+            while (run == true)
+            {
+                ConsoleKeyInfo keypress = new ConsoleKeyInfo();
+                if (tel == false)
+                {
+                    keypress = Console.ReadKey();
+                    ConsoleKey key = keypress.Key;
+                    switch (key)
+                    {
+                        case ConsoleKey.F:
+                            Console.WriteLine();
+                            tel = true;
+                            break;
+                    }
+                }
+                while (tel == true)
+                {
+                    if (Console.KeyAvailable)
+                    {
+                        keypress = Console.ReadKey();
+                    }
+                    ConsoleKey key = keypress.Key;
+                    switch (key)
+                    {
+                        case ConsoleKey.D:
+                            tel = false;
+                            break;
+                    }
+
+                    long milliseconds = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+
+                    if (milliseconds - time >= 1000)
+                    {
+                        timer++;
+                        Console.WriteLine(timer);
+                        time = milliseconds;
+                    }
+                }
+            }
+        }
+    }
+}
